434d5aa0dcdf171e2c9b0fab780565057a322b3a
GABRIEL KENNY <1703108@uad.ac.uk>
2019-10-25 19:48:23 +0100

    drone movement added

---
+++ Assets/DroneHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneHandler : MonoBehaviour
+{
+    public Vector3 droneDestination;
+    [SerializeField]
+    Camera viewingCamera;
+    [SerializeField]
+    GameObject daddyDrone;
+    [SerializeField]
+    int droneAmount;
+
+    GameObject[] drones;
+
+    private void Awake()
+    {
+        for (int i = 0; i < droneAmount; i++)
+        {
+            Instantiate(daddyDrone, daddyDrone.transform.position, Quaternion.identity);
+            
+        } 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            RaycastHit hit;
+            Ray ray = viewingCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+                if(hit.collider.GetComponent<TerrainCollider>() != null)
+                {
+                    droneDestination = hit.point;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(droneDestination, 10f);
+    }
+
+}
+
--- Assets/droneController.cs
+++ Assets/droneController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class droneController : MonoBehaviour
 {
+    DroneHandler droneHandler;
+    NavMeshAgent navMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        droneHandler = FindObjectOfType<DroneHandler>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        navMeshAgent.destination = droneHandler.droneDestination;
     }
 }
