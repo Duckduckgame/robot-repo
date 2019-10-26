2a45a380e2e722408714b180ac02a18c62d8c994
Sam <1702380@uad.ac.uk>
2019-10-26 16:24:26 +0100

    Added v rudimentary UI

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+
+    GameObject screenCanvas;
+    GameObject dronesNumCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenCanvas = GameObject.Find("ScreenCanvas");
+        dronesNumCanvas = GameObject.Find("DronesNum");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int testint = 3;
+        Text text = dronesNumCanvas.GetComponent<Text>();
+        text.text = "ROBO REVO BOYOS NUM: " + testint;
+    }
+}
