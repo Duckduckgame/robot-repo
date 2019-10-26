27dc263d8baddd7fb917657aa36e1669a18d2eb9
GABRIEL KENNY <1703108@uad.ac.uk>
2019-10-26 15:16:58 +0100

    post processing

--- Assets/Scripts/bulletHandler.cs
+++ Assets/Scripts/bulletHandler.cs
@@ -6,10 +6,19 @@ public class bulletHandler : MonoBehaviour
 {
     [SerializeField]
     float speed;
+    [SerializeField]
+    float endSize;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * speed;
+
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * endSize, Time.deltaTime * 5);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject,1f);
     }
 }
