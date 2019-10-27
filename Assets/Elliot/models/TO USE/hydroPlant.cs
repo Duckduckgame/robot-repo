91c8674aaa75c86ead192198480bd9669fd361d8
GABRIEL KENNY <1703108@uad.ac.uk>
2019-10-27 15:27:56 +0000

    power plants set

---
+++ Assets/Elliot/models/TO USE/hydroPlant.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hydroPlant : MonoBehaviour
+{
+    [SerializeField]
+    float rotSpeed;
+
+    [SerializeField]
+    bool invert = false;
+    [SerializeField]
+    Vector3 angle;
+
+    private void Start()
+    {
+
+        if (invert) rotSpeed *= -1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.rotation *= Quaternion.Euler(angle * rotSpeed);
+    }
+}
