76f3449a71a85231c09c1c6a8588b04e364617f9
GABRIEL KENNY <1703108@uad.ac.uk>
2019-10-27 17:16:06 +0000

    added win/lose

---
+++ Assets/Scripts/WinLose.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinLose : MonoBehaviour
+{
+    BoidController boidController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boidController = FindObjectOfType<BoidController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boidController.boidList.Count <= 0)
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+
+    }
+}
